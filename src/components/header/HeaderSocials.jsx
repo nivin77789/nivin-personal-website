bb45ca2f4dd3a29acf66c8b065aa13ffd1b90d2b
nivin77789 <nivin77789@gmail.com>
2023-08-19 22:43:47 +0530

    gcfh

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,19 +1,31 @@
 import React from "react";
-import { BsLinkedin } from "react-icons/bs";
-import { FaGithub } from "react-icons/fa";
-import { FiDribbble } from "react-icons/fi";
+import { FaLinkedinIn, FaGithub, FaPinterest } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com" target="_blank">
-        <BsLinkedin />
+      <a
+        href="https://www.linkedin.com/in/nivin-mathew-s-5380bb1a5/"
+        target="blank"
+      >
+        <FaLinkedinIn />
       </a>
-      <a href="https://github.com" target="_blank">
+      <a href="https://github.com/nivin77789" target="blank">
         <FaGithub />
       </a>
-      <a href="https://dribbble.com" target="_blank">
-        <FiDribbble />
+      <a
+        href="https://in.pinterest.com/nivin77789/nivin-designs/"
+        target="blank"
+      >
+        <FaPinterest />
+      </a>
+      <a href="https://www.instagram.com/nivin_mathew_s/" target="blank">
+        <FiInstagram />
+      </a>
+      <a href="https://twitter.com" target="blank">
+        <IoLogoTwitter />
       </a>
     </div>
   );
