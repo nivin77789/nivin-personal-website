f968dbbdb703e28d3f074bc79242df9fd7a3eef4
Nivin Mathew S <64590350+nivin77789@users.noreply.github.com>
2025-08-09 21:40:32 +0530

    Update Testimonials.jsx

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -19,26 +19,22 @@ const data = [
   {
     avatar: AVTR1,
     name: "Google DSC",
-    review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec dui id mi dignissim posuere feugiat vitae eros. Fusce vitae neque sit amet enim iaculis placerat eget et est. Integer.",
-  },
+    review:"Volunteered to support event planning, coordination, and technical sessions. Assisted in managing participant registrations, engaging with attendees, and promoting Google Cloud technologies. Helped create a collaborative environment for knowledge sharing among developers and tech enthusiasts."
+     },
   {
     avatar: AVTR2,
     name: "TedX",
-    review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec dui id mi dignissim posuere feugiat vitae eros. Fusce vitae neque sit amet enim iaculis placerat eget et est. Integer.",
+    review:"Designed promotional materials, event banners, and social media graphics to maintain TEDx branding standards. Worked closely with event organizers to create visually appealing content that enhanced audience engagement and overall event experience."
   },
   {
     avatar: AVTR3,
     name: "Scalar",
-    review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec dui id mi dignissim posuere feugiat vitae eros. Fusce vitae neque sit amet enim iaculis placerat eget et est. Integer.",
-  },
+    review:"Assisted in organizing community meetups and networking events. Managed registrations, coordinated with speakers, and ensured smooth event flow. Helped create a welcoming atmosphere for participants to connect, learn, and share ideas."
+      },
   {
     avatar: AVTR4,
     name: "Software Development Student Club",
-    review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec dui id mi dignissim posuere feugiat vitae eros. Fusce vitae neque sit amet enim iaculis placerat eget et est. Integer.",
+    review:"Actively contributed to organizing coding workshops, hackathons, and skill-development sessions. Assisted in logistics, participant engagement, and technical setup. Encouraged peer learning and collaboration among members to foster a strong developer community."
   },
 ];
 
