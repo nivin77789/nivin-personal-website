270989c04e1ac95590e6f63332ce19bbe771953f
Nivin Mathew S <nivinmathews@Nivins-MacBook-Air.local>
2025-10-22 03:08:07 +0530

    fas

--- src/components/about/Git-about.jsx
+++ src/components/about/Git-about.jsx
@@ -1,31 +1,31 @@
-import { React } from "react";
-import "./about.css";
+import React from "react";
+import "./gitabout.css";
 
 const Gitabout = () => {
   return (
-    <section id="about">
-      <div className="container about__container">
-        <div className="about__mes">
+    <section id="abouts">
+      <div className="container abouts__container">
+        <div className="abouts__mes">
           <div className="right">
             <div className="sub-right">
               <img
                 src={`https://leetcard.jacoblin.cool/nivin77789?ext=activity`}
-                alt="q"
+                alt="LeetCode Stats"
               />
             </div>
           </div>
         </div>
-        <div className="about__conten">
+        <div className="abouts__conten">
           <div className="right">
             <div className="sub-righ">
               <div className="img-d">
                 <img
                   src={`https://github-readme-streak-stats.herokuapp.com?user=nivin77789&theme=vision-friendly-dark&hide_border=true&date_format=M%20j%5B%2C%20Y%5D`}
-                  alt="q"
+                  alt="GitHub Streak Stats"
                 />
                 <img
                   src={`https://github-readme-stats.vercel.app/api?username=nivin77789&show_icons=true&theme=vision-friendly-dark&hide_border=true`}
-                  alt="ew"
+                  alt="GitHub Stats"
                 />
               </div>
             </div>
